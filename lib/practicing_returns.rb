056599a70b81cf45af82c82a8b4f40619acde881
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 19:39:30 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,12 +3,12 @@ require 'pry'
 def hello(array)
   i = 0
   collection = []
-  while i < array.length
-  collection <<
-    yield(array[i])
-    i += 1
-  end
-  collection
+    while i < array.length
+      collection <<
+      yield(array[i])
+       i += 1
+    end
+    collection
 end
 
 binding.pry
